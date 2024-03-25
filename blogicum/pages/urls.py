8f4f57044c670477a78947970025baef88cbbe2e
Anastasia Khanykova <Nastoman@gmail.com>
2024-03-25 21:20:11 +0300

    First

---
+++ blogicum/pages/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from .views import about, rules
+
+app_name = 'pages'
+
+urlpatterns = [
+    path('about/', about, name='about'),
+    path('rules/', rules, name='rules'),
+]
---
+++ blogicum/pages/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+def rules(request):
+    return render(request, 'pages/rules.html')
